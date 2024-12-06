b1115a19637d328aa385c42a6df0416f6cf5635f
Simon NAUD <162438554+nomisduan@users.noreply.github.com>
2024-12-06 14:53:14 +0100

    Initial Commit

---
+++ Wweather/WweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WweatherApp.swift
+//  Wweather
+//
+//  Created by Simon Naud on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WweatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

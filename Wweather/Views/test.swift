8022cad30d25759b4dde6bd5c540f6c8a977a634
Simon NAUD <162438554+nomisduan@users.noreply.github.com>
2024-12-06 16:05:22 +0100

    WeatherKit integration

---
+++ Wweather/Views/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  Wweather
+//
+//  Created by Simon Naud on 06/12/24.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test()
+}
